9775fcf57529ab5e04368b4b88cf773634f2e6b5
Daniel Kraus <daniel.kraus@mailbox.org>
2018-01-24 16:53:32 +0100

    Link SSH config

--- install.sh
+++ install.sh
@@ -8,3 +8,5 @@ source "$dotfiles_path/runcom/.bash_profile"
 ln -sfv "$dotfiles_path/git/.gitconfig" ~
 ln -sfv "$dotfiles_path/git/.gitconfig_retest" ~
 ln -sfv "$dotfiles_path/git/.gitignore_global" ~
+
+ln -sfv ~/Dropbox/kruse/dotfiles/ssh/config ~/.ssh
