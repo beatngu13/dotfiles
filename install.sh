00dd495c00d5029ffc0fb3e56d18379815664bfe
Daniel Kraus <daniel.kraus@mailbox.org>
2018-03-18 22:57:59 +0100

    Clean up installation process

--- install.sh
+++ install.sh
@@ -10,58 +10,8 @@ while true; do
     kill -0 "$$" || exit
 done &>/dev/null &
 
-# Install Homebrew.
-ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-
-brew tap caskroom/versions
-brew tap caskroom/cask
-
-# Homebrew apps.
-brew_apps=(
-    bash-completion
-    git
-    jenv
-    maven
-    pdfgrep
-)
-
-brew install "${brew_apps[@]}"
-
-# Homebrew-Cask apps.
-cask_apps=(
-    adobe-acrobat-reader
-    atom
-    diffmerge
-    dropbox
-    eclipse-java
-    firefox
-    gimp
-    google-drive-file-stream
-    java
-    java8
-    java6
-    keepassxc
-    keka
-    libreoffice
-    mactex
-    microsoft-office
-    pyenv-virtualenv
-    skype
-    slack
-    sourcetree
-    spectacle
-    spotify
-    spotmenu
-    vagrant
-    virtualbox
-    whatsapp
-)
-
-brew cask install "${cask_apps[@]}"
-
-# Install manually.
-#   Microsoft Remote Desktop
-#   Logitech Options
+# Install Homebrew and Homebrew cask including apps.
+source install/brew.sh
 
 # Configure macOS.
-source macos/defaults.sh
+source install/macos.sh
