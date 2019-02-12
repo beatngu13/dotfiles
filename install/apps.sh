179d5b432f1a25602eca1c9526e66bed2959ddbb
Daniel Kraus <daniel.kraus@mailbox.org>
2019-02-12 16:44:13 +0100

    Address `TODO`s

--- install/apps.sh
+++ install/apps.sh
@@ -7,66 +7,65 @@ brew tap caskroom/versions
 brew tap caskroom/cask
 brew tap AdoptOpenJDK/openjdk
 
-# TODO Sort apps logically (e.g. Clojure requires Java).
-# TODO Continue installations when a single app fails.
-# TODO Add build tools for other languages (Leiningen, sbt, etc.)
+# TODO Add build tools for other languages (Leiningen, sbt, etc.).
 
 # Homebrew apps.
-brew_apps=(
-    bash-completion
-    clojure
-    geckodriver
-    git
-    gradle
-    graphviz
-    jenv
-    mas
-    maven
-    neofetch
-    pandoc
-    pdfgrep
-    pyenv-virtualenv
-    scala
-)
 
-brew install "${brew_apps[@]}"
+brew install bash-completion
+brew install clojure
+brew install geckodriver
+brew install git
+brew install gradle
+brew install graphviz
+brew install jenv
+brew install mas
+brew install maven
+brew install neofetch
+brew install pandoc
+brew install pdfgrep
+brew install pyenv-virtualenv
+brew install scala
 
 # Homebrew-Cask apps.
-cask_apps=(
-    adobe-acrobat-reader
-    adoptopenjdk
-    adoptopenjdk8
-    chromedriver
-    diffmerge
-    dropbox
-    eclipse-installer
-    firefox
-    gimp
-    google-chrome
-    google-drive-file-stream
-    gpg-suite
-    intellij-idea-ce
-    jd-gui
-    keepassxc
-    keka
-    libreoffice-still
-    mactex
-    microsoft-office
-    skype
-    slack
-    sourcetree
-    spectacle
-    spotify
-    spotmenu
-    vagrant
-    virtualbox
-    visual-studio-code
-    whatsapp
-)
 
-brew cask install "${cask_apps[@]}"
+brew cask install adobe-acrobat-reader
+brew cask install adoptopenjdk
+brew cask install adoptopenjdk8
+brew cask install chromedriver
+brew cask install diffmerge
+brew cask install dropbox
+brew cask install eclipse-installer
+brew cask install firefox
+brew cask install gimp
+brew cask install google-chrome
+brew cask install google-drive-file-stream
+brew cask install gpg-suite
+brew cask install intellij-idea-ce
+brew cask install jd-gui
+brew cask install keepassxc
+brew cask install keka
+brew cask install libreoffice-still
+brew cask install mactex
+brew cask install microsoft-office
+brew cask install skype
+brew cask install slack
+brew cask install sourcetree
+brew cask install spectacle
+brew cask install spotify
+brew cask install spotmenu
+brew cask install vagrant
+brew cask install virtualbox
+brew cask install visual-studio-code
+brew cask install whatsapp
+
+# Homebrew misc (require apps from above).
+
+# Require Java.
+brew install clojure
+brew install scala
 
 # Mac App Store apps.
+
 # Microsoft Remote Desktop 10.
 mas install 1295203466
 # Keynote.
