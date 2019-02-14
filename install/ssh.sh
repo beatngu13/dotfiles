aa783ee9446e1e23067777f16d53865a64fa36e1
Daniel Kraus <daniel.kraus@mailbox.org>
2019-02-14 19:24:07 +0100

    Add comment

--- install/ssh.sh
+++ install/ssh.sh
@@ -5,4 +5,5 @@ ln -sfv "${DROPBOX_DOTFILES_PATH}"/ssh/config ~/.ssh/config
 ln -sfv "${DROPBOX_DOTFILES_PATH}"/ssh/id_rsa ~/.ssh/id_rsa
 ln -sfv "${DROPBOX_DOTFILES_PATH}"/ssh/id_rsa.pub ~/.ssh/id_rsa.pub
 
+# Permanently add SSH key (will prompt for passphrase).
 ssh-add -K ~/.ssh/id_rsa
