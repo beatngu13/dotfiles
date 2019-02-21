175d430f20919013d9ad7af453a6adc4fc8bf94d
Daniel Kraus <daniel.kraus@mailbox.org>
2019-02-21 17:05:53 +0100

    Set SSH permissions

--- install/ssh.sh
+++ install/ssh.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
 mkdir ~/.ssh/
+
 ln -sfv "${DROPBOX_DOTFILES_PATH}"/ssh/config ~/.ssh/config
 ln -sfv "${DROPBOX_DOTFILES_PATH}"/ssh/id_rsa ~/.ssh/id_rsa
 ln -sfv "${DROPBOX_DOTFILES_PATH}"/ssh/id_rsa.pub ~/.ssh/id_rsa.pub
+
+# Recursively set rw user permissions.
+chmod -R 600 ~/.ssh/
 
 # Permanently add SSH key (will prompt for passphrase).
 ssh-add -K ~/.ssh/id_rsa
