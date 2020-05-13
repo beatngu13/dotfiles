8c223f754c7d09a68678702ee5268392567337af
Daniel Kraus <daniel.kraus@mailbox.org>
2020-05-13 20:19:26 +0200

    Update default repos

--- install/repos.sh
+++ install/repos.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-# Private.
-
 mkdir -p "${PRIVATE_WORKSPACE_PATH}"
 cd "${PRIVATE_WORKSPACE_PATH}"
 
@@ -9,24 +7,6 @@ git clone git@bitbucket.org:beatngu13/master-thesis.git
 git clone git@bitbucket.org:beatngu13/papers.git
 
 git clone git@github.com:beatngu13/pdf-zoom-wizard.git
+git clone git@github.com:beatngu13/knapsack-problem.git
 
-# ReTest.
-
-mkdir -p "${RETEST_WORKSPACE_PATH}"
-cd "${RETEST_WORKSPACE_PATH}"
-
-git clone git@bitbucket.org:retest/bugsubmitter.git
-git clone git@bitbucket.org:retest/review.git
-git clone git@bitbucket.org:retest/surili.git
-git clone git@bitbucket.org:retest/reports-2021-kmuinnovativ-surili.git
-git clone git@bitbucket.org:retest/website.git
-
-git clone git@github.com:retest/clabot-config.git
-git clone git@github.com:retest/docs.git
-git clone git@github.com:retest/gui-state-machine-api.git
-git clone git@github.com:retest/homebrew-tap.git
-git clone git@github.com:retest/rebazer.git
-git clone git@github.com:retest/recheck.git
-git clone git@github.com:retest/recheck.cli.git
-git clone git@github.com:retest/recheck-web.git
-git clone git@github.com:retest/surili-commons.git
+git clone git@github.com:junit-pioneer/junit-pioneer.git
