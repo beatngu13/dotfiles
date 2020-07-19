ac73d5eb4852e4c75085311ac334935fea5440d6
Daniel Kraus <daniel.kraus@mailbox.org>
2020-07-19 14:30:24 +0200

    Also show volume in menu

--- install/macos.sh
+++ install/macos.sh
@@ -32,7 +32,8 @@ defaults write com.apple.systemuiserver menuExtras -array \
     "/System/Library/CoreServices/Menu Extras/Clock.menu", \
     "/System/Library/CoreServices/Menu Extras/Battery.menu", \
     "/System/Library/CoreServices/Menu Extras/AirPort.menu", \
-    "/System/Library/CoreServices/Menu Extras/Displays.menu"
+    "/System/Library/CoreServices/Menu Extras/Displays.menu", \
+    "/System/Library/CoreServices/Menu Extras/Volume.menu"
 
 # Show battery percentage in menu bar.
 defaults write com.apple.menuextra.battery ShowPercent YES
