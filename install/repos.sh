6495b080af54e2f006e82825f1949a3f753227a8
Daniel Kraus <daniel.kraus@mailbox.org>
2020-12-26 19:14:14 +0100

    Update repos
    Use Pioneer fork and sort alphabetically.

--- install/repos.sh
+++ install/repos.sh
@@ -3,10 +3,6 @@
 mkdir -p "${PRIVATE_WORKSPACE_PATH}"
 cd "${PRIVATE_WORKSPACE_PATH}"
 
-git clone git@bitbucket.org:beatngu13/master-thesis.git
-git clone git@bitbucket.org:beatngu13/papers.git
-
-git clone git@github.com:beatngu13/pdf-zoom-wizard.git
+git clone git@github.com:beatngu13/junit-pioneer.git
 git clone git@github.com:beatngu13/knapsack-problem.git
-
-git clone git@github.com:junit-pioneer/junit-pioneer.git
+git clone git@github.com:beatngu13/pdf-zoom-wizard.git
