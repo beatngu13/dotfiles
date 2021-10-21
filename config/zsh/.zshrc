1465a45fd6216bd809371df76c17b29038e2e4c9
Daniel Kraus <daniel.kraus@mailbox.org>
2021-10-22 01:11:07 +0200

    Add function to fix GraalVM quarantine

--- config/zsh/.zshrc
+++ config/zsh/.zshrc
@@ -13,7 +13,6 @@ export EDITOR="${CONFIG_PATH}"/zsh/editor.sh
 export LANG=en_US.UTF-8
 
 # Set up path.
-# TODO xattr -r -d com.apple.quarantine /Library/Java/JavaVirtualMachines/graalvm-ce-java11-21.3.0/Contents/Home
 export GRAALVM_HOME=/Library/Java/JavaVirtualMachines/graalvm-ce-java11-21.3.0/Contents/Home/
 export PATH=\
 "${PATH}":\
--- system/.java
+++ system/.java
@@ -33,3 +33,18 @@ function mvn_degraph() {
             -Dincludes="${package}" \
             -DshowVersions
 }
+
+# Remove quarantine from GraalVM.
+# See "On Software Notarization": https://www.graalvm.org/docs/getting-started/macos/#installation-notes
+function graalvm_quarantine() {
+    graalvm_dir="{1}"
+    jvms_path=/Library/Java/JavaVirtualMachines/
+
+    sudo -v
+
+    mv "${jvms_path}${graalvm_dir}" /tmp/
+    xattr -r -d com.apple.quarantine /tmp/"${graalvm_dir}"/Contents/Home
+    mv /tmp/"${graalvm_dir}" "${jvms_path}"
+
+    echo 'Do not forget to update GRAALVM_HOME and run jenv add'
+}
