e064544d704ae8bb3ff62964f8e3c37e126ecffc
Daniel Kraus <daniel.kraus@mailbox.org>
2023-04-05 23:25:03 +0200

    Update repos

--- install/repos.sh
+++ install/repos.sh
@@ -3,10 +3,8 @@
 mkdir -p "${PRIVATE_WORKSPACE_PATH}"
 cd "${PRIVATE_WORKSPACE_PATH}"
 
-git clone git@github.com:beatngu13/junit-pioneer.git
-cd junit-pioneer/
-git remote add junit-pioneer git@github.com:junit-pioneer/junit-pioneer.git
-cd -
+git clone git@github.com:junit-pioneer/junit-pioneer.git
 
 git clone git@github.com:beatngu13/knapsack-problem.git
+git clone git@github.com:beatngu13/playground.git
 git clone git@github.com:beatngu13/pdf-zoom-wizard.git
